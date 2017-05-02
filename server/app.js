7829957b32b2d1dfdd2ada297d1ebe6e61775934
Alexey Kureev <a.g.kureev@gmail.com>
2017-05-02 22:43:40 +0200

    Clean version

--- server/app.js
+++ server/app.js
@@ -1,13 +1,8 @@
 const fs = require('fs');
-const https = require('https');
 const path = require('path');
-const privateKey  = fs.readFileSync(path.join(__dirname, './key.pem'), 'utf8');
-const certificate = fs.readFileSync(path.join(__dirname, './cert.pem'), 'utf8');
-const credentials = {key: privateKey, cert: certificate};
 const cors = require('cors');
 const express = require('express');
 const app = express();
-const { Observable } = require('rxjs');
 const getPuns = require('./getPuns');
 const suggestKeywords = require('./suggestKeywords');
 
@@ -16,18 +11,15 @@ app.use(cors());
 app.get('/suggest-keywords', (req, res) => {
   const q = req.query.q;
   const keywords = suggestKeywords(q);
-  res.send(keywords);
+  res.json({ keywords });
 });
 
 app.get('/puns', (req, res) => {
   const keywords = req.query.q ? req.query.q.split(',') : null;
   const puns = getPuns(keywords);
-  res.send(puns); 
+  res.json(puns);
 });
 
-
-const httpsServer = https.createServer(credentials, app);
-
-httpsServer.listen(4201);
+app.listen(8888);
 
 console.log("server running");
